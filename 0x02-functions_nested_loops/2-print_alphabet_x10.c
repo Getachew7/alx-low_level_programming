ded03ce626fe9d5af2761b82e697ade2e1d48ad4
Getachew <getchyaz7@gmail.com>
2022-07-05 05:44:56 -0700

    another

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,10 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 {
-		for (letter = 'a'; letter <= 'z'; letter++)						_putchar(letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
 		_putchar('\n');
 										}
 }
