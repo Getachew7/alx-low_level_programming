d87a9c3557b277ca896ae45fb389419a90cffa73
Getachew <getchyaz7@gmail.com>
2022-07-26 06:17:15 -0700

    arry mallo

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
-
-
 /**
-
 * create_array -> creating array
-
 * @size: size of array to be created
-
 * @c: character an array is initialized with
-
 * Return: a pointer to an array
-
 */
 
 char *create_array(unsigned int size, char c)
